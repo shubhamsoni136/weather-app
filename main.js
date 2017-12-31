ebb0f96125952fc698b2afdb908a87f1dee6c089
shubham <shubhamsoni136@gmail.com>
2017-12-31 21:26:10 +0530

    files added

---
+++ main.js
@@ -0,0 +1,19 @@
+Mousetrap.bind('/',submit_req());
+var submit = document.getElementById('submit_btn');
+ submit.onclick =function (){
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+      if(request.status===200){
+        alert((JSON.parse(request.responseText).list[0].main.temp_max)/10);
+      }else{
+        alert(request.responseText.toString());
+      }
+    }
+  };
+  var loc = document.getElementById("locationTextField").value;
+  document.getElementById("locationTextField").value="";
+  request.open("POST","/weather",true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({"data":loc}));
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+var request = require('request');
+var express = require("express");
+var Mousetrap = require('mousetrap');
+var bodyParser = require("body-parser");
+var path = require("path");
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/",function(req,res){
+  res.sendFile(path.join(__dirname , 'home.html'));
+});
+app.post("/weather",function(req,res){
+  var d = req.body.data;
+  request.get({url:"http://api.openweathermap.org/data/2.5/forecast", qs:{"q":d,
+    "APPID":"d604753e308dfbb21737c987a0986651"}},function(error,response,body){
+      res.send(body);
+    });
+});
+app.get("/logo.png",function(req,res){
+  res.sendFile(path.join(__dirname , 'logo.png'));
+});
+app.get("/main.js",function(req,res){
+  res.sendFile(path.join(__dirname , 'main.js'));
+});
+app.get("/icon.png",function(req,res){
+  res.sendFile(path.join(__dirname , 'icon.png'));
+});
+app.listen(8080,function(){
+  console.log("the web app is listening at port 8080")
+});
